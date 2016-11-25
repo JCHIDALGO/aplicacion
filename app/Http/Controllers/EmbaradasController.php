e04f966d125108013813a20bd78b6833e1bbc583
root <root@localhost.localdomain>
2016-11-24 22:30:23 -0500

    query que consulta en base a rango de edad

--- app/Http/Controllers/EmbaradasController.php
+++ app/Http/Controllers/EmbaradasController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Embarazada;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 class EmbaradasController extends Controller
 {
     /**
@@ -24,7 +26,41 @@ class EmbaradasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+public function edad($from, $to){
+    $usuariosEntre20Y30 = Embarazada::whereBetween(DB::raw('TIMESTAMPDIFF(YEAR,tembarazadas.fnacimiento,CURDATE())'), [$from, $to])->get();
 
+    /*$users = DB::table('users')
+                     ->select(DB::raw('count(*) as user_count, status'))
+                     ->where('status', '<>', 1)
+                     ->groupBy('status')
+                     ->get(); */
+
+//$hoy= new Carbon;
+
+//$fechas_embarazadas = Embarazada::select('');
+
+/*foreach ($fechas_embarazadas as $fecha) {
+    
+$date = Carbon::createFromFormat('Y-m-d',$fecha->fnacimiento)->age;
+
+$edades[]=['expediente'=>$fecha->expediente,'edad'=>$date];
+} 
+*/
+
+return response()->json(['ranog_edades'=>$usuariosEntre20Y30]);
+
+}
+
+public function riesgo(){
+$bajoRiesgo= Embarazada::where('riesgo','>=',0)->where('riesgo','<',5)->get();
+$medianoRiesgo= Embarazada::where('riesgo','>=',5)->where('riesgo','<',8)->get();
+$altoRiesgo= Embarazada::where('riesgo','>=',8)->get();
+$totalBajoRiesgo = $bajoRiesgo->count();
+$totalMedianoRiesgo = $medianoRiesgo->count();
+$totalAltoRiesgo = $altoRiesgo->count();
+return response()->json(['Bajo Riesgo'=>$totalBajoRiesgo,'Mediano Riesgo'=>$totalMedianoRiesgo,'Alto Riesgo'=>$totalAltoRiesgo ]);
+
+}
     public function detalle($id){
 
 
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {     return view('welcome'); });
 Route::resource('Embarazadas','EmbaradasController');
 Route::resource('Puerperas','PuerperasController');
 Route::get('DetalleEmbarazada/{expediente}','EmbaradasController@detalle');
+Route::get('Riesgo','EmbaradasController@riesgo');
+Route::get('Edad/{from}/{to}','EmbaradasController@edad');
+Route::get('Embarazadas/Municipio/{}','EmbaradasController@municipio');
